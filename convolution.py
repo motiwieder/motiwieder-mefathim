3f1846ebc35e585585da91f69a26478b432aa530
motiwieder <93468599+motiwieder@users.noreply.github.com>
2021-11-23 16:00:26 +0200

    Add files via upload

--- convolution.py
+++ convolution.py
@@ -1,10 +1,29 @@
 import numpy as np
-new_image = np.array([[1, 2, 3,8,0] , [4, 5,6,3, 6] , [2,2,2,3,6] , [5,8,7,5,4] , [4,6,4,3,8]])
+
+
 f = np.random.uniform(size=(3,5,5))
- 
-def convolucion(f):
-#	new_image = np.array([[1, 2, 3,8,0] , [4, 5,6,3, 6] , [2,2,2,3,6] , [5,8,7,5,4] , [4,6,4,3,8]])
-#	f = np.random.uniform(size=(3,5,5))
+f = f.T
+image = np.random.uniform(size=(6,28,28))
+
+def new_image(image):
+
+	new_image = []
+
+	for k in range(image.shape[2]):
+
+		for i in range(image.shape[0]-f.shape[0]+1):
+
+			for j in range(image.shape[1]-f.shape[1]+1):
+				new_image.append(image[:,:,k][i:i+f.shape[0],j:j+f.shape[1]])
+
+	new_image = np.array(new_image)
+	new_image.resize((image.shape[2],int(new_image.shape[0]/image.shape[2]),new_image.shape[1],new_image.shape[2]))
+	new_image.shape
+	return new_image
+
+def convolucion(f,new_image):
+	new_image = np.array([[1, 2, 3,8,0] , [4, 5,6,3, 6] , [2,2,2,3,6] , [5,8,7,5,4] , [4,6,4,3,8]])
+	f = np.random.uniform(size=(3,5,5))
 	filter_output = []
 	
 	for i in range(len(new_image)):
@@ -18,4 +37,6 @@ def convolucion(f):
 	print(filter_output)
 #	filter_output_sigmoid = sigmoid(filter_output)
 
-convolucion(f)
+
+ni = new_image(image)
+convolucion(f,ni)
